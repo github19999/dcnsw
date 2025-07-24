325d9eca76f2a39c020c5ff87cbd790df607fc93
github19999 <151017823+github19999@users.noreply.github.com>
2025-07-24 20:47:43 +0800

    Update install-dcnsb2.sh

--- install-dcnsb2.sh
+++ install-dcnsb2.sh
@@ -6,7 +6,7 @@ echo "🔄 更新系统..."
 apt update -y && apt upgrade -y
 
 echo "📦 安装必要组件..."
-apt install -y curl wget unzip git sudo nano vim
+apt install -y curl wget unzip git sudo nano vim docker.io docker-compose
 
 echo "🕒 设置时区为上海..."
 timedatectl set-timezone Asia/Shanghai
